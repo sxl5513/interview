a578439af8c4e5e3233103036346ef02dec2ba60
anber <526460465@qq.com>
2018-10-27 12:57:38 +0800

    用循环方法 建立出大链表

--- src/main/java/com/anber/interview/recursion/LinkedListCreator.java
+++ src/main/java/com/anber/interview/recursion/LinkedListCreator.java
@@ -28,10 +28,35 @@ public class LinkedListCreator {
         return firstNode;
     }
 
+    public Node createLargeLinkedList(int size) {
+        Node prev = null;
+        Node head = null;
+        for (int i = 1; i <= size; i++) {
+            Node node = new Node(i);
+            if (prev != null) {
+                prev.setNext(node);
+            } else {
+                head = node;
+            }
+            prev = node;
+        }
+        /**
+         * 在运行 head = node时  两个变量指向内存地址相同
+         * 当 prev = node时 head prev 和 node 指向的内存地址相同
+         * node = new Node（）时  node指向新的内存地址
+         * 而prev 和 node  指向内存地址是一直相同
+         * 所以当prev 对数据更改时 node的数据也在更改 两个变量中的next对象 指向的内存地址 是之前刚刚new出的node()
+         * 依次往下
+         */
+        return head;
+    }
+
     public static void main(String[] args) {
         LinkedListCreator creator = new LinkedListCreator();
         Node.printLinkedList(creator.createLinkedList(new ArrayList<>()));
         Node.printLinkedList(creator.createLinkedList(Arrays.asList(1)));
         Node.printLinkedList(creator.createLinkedList(Arrays.asList(1,2,3,4,5)));
+
+        creator.createLargeLinkedList(100);
     }
 }
